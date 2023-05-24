3acc876e135578fe949269984ddc42cd79da5d5a
Igor Todorovski <itodorov@ca.ibm.com>
2023-05-24 13:29:06 -0400

    add polling to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,14 @@
 node('linux')
 {
+   
+  stage (Poll) {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libgpgerrorport.git"]]])
+        }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libgpgerrorport.git'), string(name: 'PORT_DESCRIPTION', value: 'This contains common error codes and error handling' )]
   }
