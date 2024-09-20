db644da26b0e66d791f6125b5d5427e114aca847
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:36 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libgpgerrorport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/libgpgerrorport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libgpgerrorport.git'), string(name: 'PORT_DESCRIPTION', value: 'This contains common error codes and error handling' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libgpgerrorport.git'), string(name: 'PORT_DESCRIPTION', value: 'This contains common error codes and error handling' )]
   }
 }
